091a8a15b91ecf818c1fe65d1a5f1e2fab17f5e2
Admin <shadow00713@yandex.ru>
2024-02-21 16:49:33 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <windows.h>
+
+int main()  
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "ru");
+	/*std::cout << " катерина \n" <<17 <<" \nмилионером \n" << "потому чо € их вкусно пожариал \n" <<"пюрешка" ;
+	int a = 1;
+	a = 3;
+	std::cout << a + 1 << a * a << a - 2 << " " << a;
+	a = 4; 
+	int a = 1;
+	float b = 3.2;
+	double c = 5.3;
+	std::cout << a << " " << b << " ";
+	int a;
+	std::cin >> a;
+	std::cout << a;
+	setlocale(LC_ALL, "ru");
+	int a;
+	int b;
+	int c;
+	
+
+	std::cout << "¬ведите первое число ";
+	std::cin >> a;
+	std::cout << "¬ведите второе число ";
+	std::cin >> b;
+	std::cout << "¬ведите третье число ";
+	std::cin >> c;
+
+
+	if (a != b && a!= c && b !=c)
+	{
+		if (a > b && b>c) 
+		{
+			std::cout << a << b << c;
+		}
+		if (a > c && c > b)
+		{
+			std::cout << a << c << b;
+		}
+		else if (b > a && a> c)
+		{
+			std::cout << b << a << c;
+		}
+		else if (b > c && c > a)
+		{
+			std::cout << b << c << a;
+		}
+		else if (c > a && a > b)
+		{
+			std::cout << c << a << b;
+		}
+		else if (c > b && b > a)
+		{
+			std::cout << c << b << a;
+		}
+	}
+	else
+	{
+		std::cout << "„исла равны";
+	}*/
+
+
+
+
+	return 0;
+}
